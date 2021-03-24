bc60a22756dc447807c59c6b176231c35d86266b
boris-careev <boris@careevolution.com>
2021-03-23 20:29:47 -0500

    use https://mdhorg.ce.dev/MyDataHelps/ as base URL

--- app/services/urlService.js
+++ app/services/urlService.js
@@ -4,7 +4,8 @@
 		// Update based on what site you are using (b3, prod, localhost, etc.)
         
 		//var hostBaseUrl = "http://localhost:64531/WebClient/";
-		var hostBaseUrl = "http://localhost/WebClientTest.Adapter1.WebClient/";
+		//var hostBaseUrl = "http://localhost/WebClientTest.Adapter1.WebClient/";
+		var hostBaseUrl = "https://mdhorg.ce.dev/MyDataHelps/";
 
 		var baseUrl = MyDataHelps.baseUrl;
 		var apiUrl = hostBaseUrl + "api/v1/delegated/";
